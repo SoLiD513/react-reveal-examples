bc035093315f220d40ecb84fcd3a838eb543b070
Jesse McKinney <jessman51386@gmail.com>
2020-02-18 12:38:12 -0500

    yay' '

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Fade from "react-reveal/Fade";
+import Flip from "react-reveal/Flip";
+import Rotate from "react-reveal/Rotate";
+import Zoom from "react-reveal/Zoom";
+import Bounce from "react-reveal/Bounce";
+import Roll from "react-reveal/Roll";
+import Slide from "react-reveal/Slide";
+import LightSpeed from "react-reveal/LightSpeed";
+import Flash from "react-reveal/Flash";
+import HeadShake from "react-reveal/HeadShake";
+import Jello from "react-reveal/Jello";
+import Jump from "react-reveal/Jump";
+import Pulse from "react-reveal/Pulse";
+import RubberBand from "react-reveal/RubberBand";
+import Shake from "react-reveal/Shake";
+import Spin from "react-reveal/Spin";
+import Swing from "react-reveal/Swing";
+import Tada from "react-reveal/Tada";
+import Wobble from "react-reveal/Wobble";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Fade forever={true}>
+        <h1>Fade Content</h1>
+      </Fade>
+      <br />
+      <Flip forever={true}>
+        <h1>Flip Content</h1>
+      </Flip>
+      <br />
+      <Rotate forever={true}>
+        <h1>Rotate Content</h1>
+      </Rotate>
+      <br />
+      <Zoom forever={true}>
+        <h1>Zoom Content</h1>
+      </Zoom>
+      <br />
+      <Bounce forever={true}>
+        <h1>Bounce Content</h1>
+      </Bounce>
+      <br />
+      <Slide forever={true}>
+        <h1>Slide Content</h1>
+      </Slide>
+      <br />
+      <Roll forever={true}>
+        <h1>Roll Content</h1>
+      </Roll>
+      <br />
+      <LightSpeed forever={true}>
+        <h1>LightSpeed Content</h1>
+      </LightSpeed>
+      <br />
+      <Flash forever={true}>
+        <h1>Flash Content</h1>
+      </Flash>
+      <br />
+      <HeadShake forever={true}>
+        <h1>HeadShake Content</h1>
+      </HeadShake>
+      <br />
+      <Jello forever={true}>
+        <h1>Jello Content</h1>
+      </Jello>
+      <br />
+      <Jump forever={true}>
+        <h1>Jump Content</h1>
+      </Jump>
+      <br />
+      <Pulse forever={true}>
+        <h1>Pulse Content</h1>
+      </Pulse>
+      <br />
+      <RubberBand forever={true}>
+        <h1>RubberBand Content</h1>
+      </RubberBand>
+      <br />
+      <Shake forever={true}>
+        <h1>Shake Content</h1>
+      </Shake>
+      <br />
+      <Spin forever={true}>
+        <h1>Spin Content</h1>
+      </Spin>
+      <br />
+      <Swing forever={true}>
+        <h1>Swing Content</h1>
+      </Swing>
+      <br />
+      <Tada forever={true}>
+        <h1>Tada Content</h1>
+      </Tada>
+      <br />
+      <Wobble forever={true}>
+        <h1>Wobble Content</h1>
+      </Wobble>
+    </>
   );
 }
 
